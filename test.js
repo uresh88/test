e457f91e3d83006091eb896a8eb923a792bee157
uresh <uresh88@users.noreply.github.com>
2018-10-12 20:48:22 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,9 +3,13 @@
    var t= document.getElementById('test');
 var i = 0
 while(true){
-t.innerHTML = (i++) %10
-if(i=10){
-i=0
-}
+ 
+ setTimeout(()=>{
+   t.innerHTML = (i++) %10
+   if(i=10){
+   i=0
+   }
+ },10)
+
 }
   });
